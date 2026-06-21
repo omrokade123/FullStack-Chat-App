b5d4e7452dc282254cecf1ecc02c0b488ec87fab
Om Rokade <Omrokade904@gmail.com>
2026-06-21 16:51:01 +0530

    new commit

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -29,7 +29,7 @@ io.on("connection",(socket) => {
     socket.on("disconnect",() => {
         console.log("A user disconnected",socket.id);
         delete userSocketMap[userId];
-        io.emit("getOnlineUser",Object.keys(userSocketMap));
+        io.emit("getOnlineUsers",Object.keys(userSocketMap));
     });
 })
 
