89558878fa6c9200ba5b33c62fe5e7bfe3daa05e
Om Rokade <Omrokade904@gmail.com>
2026-06-21 16:36:02 +0530

    new commit

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const genrateToken = (userID,res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //MS - No of days an login last for
         httpOnly: true, // prevent an XSS attack cross-site scripting attacks
         sameSite: "none",// CSRF attacks cross-site request foregery attack
-        secure: process.env.NODE_ENV"
+        secure: process.env.NODE_ENV !== "development"
     })
 
     return token;
